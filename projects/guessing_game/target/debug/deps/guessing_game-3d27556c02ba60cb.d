f70f103ad96bc446096bcab411290e51434af5e2
ddalbarracin <danield.albarracin@gmail.com>
2024-08-22 19:23:54 -0300

    Rust: Guess Game
    Signed-off-by: ddalbarracin

---
+++ projects/guessing_game/target/debug/deps/guessing_game-3d27556c02ba60cb.d
@@ -0,0 +1,5 @@
+/home/ddalbarracin/Documents/code/repos/rust/projects/guessing_game/target/debug/deps/guessing_game-3d27556c02ba60cb: src/main.rs
+
+/home/ddalbarracin/Documents/code/repos/rust/projects/guessing_game/target/debug/deps/guessing_game-3d27556c02ba60cb.d: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/deps/unicode_ident-542f5604bbfe0ac2.d
@@ -0,0 +1,8 @@
+/home/ddalbarracin/Documents/code/repos/rust/projects/guessing_game/target/debug/deps/libunicode_ident-542f5604bbfe0ac2.rmeta: /home/ddalbarracin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/ddalbarracin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/ddalbarracin/Documents/code/repos/rust/projects/guessing_game/target/debug/deps/libunicode_ident-542f5604bbfe0ac2.rlib: /home/ddalbarracin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/ddalbarracin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/ddalbarracin/Documents/code/repos/rust/projects/guessing_game/target/debug/deps/unicode_ident-542f5604bbfe0ac2.d: /home/ddalbarracin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/ddalbarracin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/ddalbarracin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs:
+/home/ddalbarracin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs:
